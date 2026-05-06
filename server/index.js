c03b3fed2c6f857c4f85498a5bca3f03eed26dbb
Jayasurya S <jayasuryas@Jayasuryas-MacBook-Air.local>
2026-05-06 14:55:29 +0530

    Fix Express v5 routing crash

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.get('/api/health', (req, res) => {
 const path = require('path')
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
